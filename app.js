4c2500a3557a6c8803db42fbcad6dd477089097a
TranTu <trananhtu210606@gmail.com>
2026-06-23 15:57:36 +0700

    hello world voi express

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-console.log("hello world")
-console.log("tran anh tu")
+const express = require("express")
+
+const app = express();
+
+const port = 8080;
+
+app.get('/',(req,res)=>{
+    res.send("hello world ")
+})
+app.get('/trananhtu',(req,res)=>{
+    res.send("hello tran anh tu n")
+})
+
+
+app.listen(8080,()=>{
+    console.log(`my app is running on port : ${port}`)
+})
